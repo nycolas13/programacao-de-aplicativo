a2de4aa91b6a3333d8194ec805097a025c2d0e20
Nycolas <nycolasla13@gmail.com>
2026-02-18 12:45:52 -0300

    Primeira aula de programação

---
+++ aula01.py
@@ -0,0 +1,10 @@
+numero = 250 # variável int
+numero_quebrado = 2.56 # variável float
+joao_ta_dormindo = True # variável booleana
+texto = "aula inicial de programação" # variável de texto
+
+
+print(joao_ta_dormindo)
+print(numero)
+print(numero_quebrado)
+print(texto)
---
+++ aula02.py
@@ -0,0 +1,10 @@
+numero = 600000000 # variável int
+numero_quebrado = 4.05 # variável float
+nycolas_ta_trabalhando = True # variável blooana
+texto = "práticando a escrever código" # variável de texto
+
+
+print(numero)
+print(texto)
+print(nycolas_ta_trabalhando)
+print(numero_quebrado)
