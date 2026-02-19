e800438b9070853865d92c077c742292d8c19ad4
Nycolas Antônio <nycolasla13@gmail.com>
2026-02-19 10:05:08 -0300

    Aula pratica de programação de aplicativo

---
+++ animal.py
@@ -0,0 +1,10 @@
+nome = "gato" # variável string
+idade = 13 # variável inteira
+peso = 3. # variável float
+sexo_masculino = True # variável string
+
+
+print(nome)
+print(peso)
+print(idade)
+print(sexo_masculino)
