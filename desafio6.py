f00fef379637c43e7f5fc55ec0a100a170f15a12
Nycolas Antônio <nycolasla13@gmail.com>
2026-03-24 09:59:12 -0300

    terminei todas as atividades

--- desafio6.py
+++ desafio6.py
@@ -3,16 +3,17 @@ lista_autorizados = ["Alice","Bob","Carlos"]
 nome = input("Digite o nome de um pesquisador: ")
 
 # Verifição de Existencia
-if nome == lista_autorizados:
-    lista_autorizados.index(2)
-    print(f"Acesso Permitido! O pesquisador {nome} está na posição {lista_autorizados}.")
-usuario = input("Você deseja remover esse pesquisador da lista (S/N): ")
-if usuario == "S":
-    lista_autorizados.remove(2)
-    print(f"lista atualizada {lista_autorizados}")
-elif lista_autorizados:
+if nome in lista_autorizados:
+    indice = lista_autorizados.index(nome)
+    print(f"Acesso Permitido! O pesquisador {nome} está na posição {indice}.")
+
+    remover = input("Você deseja remover esse pesquisador da lista (S/N): ")
+    if remover == "S":
+        lista_autorizados.remove(nome)
+        print(f"lista atualizada {lista_autorizados}")
+else:
     print(f"Acesso Negado! O pesquisador {nome} não foi encontrado.")
-usuario = input("Deseja cadastrar esse novo pesquisador (S/N): ")
-if usuario == "S":
-    lista_autorizados.apped()
-    
+    adicionar = input("Deseja cadastrar esse novo pesquisador (S/N): ")
+    if adicionar == "S":
+        lista_autorizados.append(nome)
+        print(f"lista atual{lista_autorizados}")
