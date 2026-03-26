ce1878538b24d07016f7aa61d252b2aab8a1eb90
Nycolas Antônio <nycolasla13@gmail.com>
2026-03-26 10:10:08 -0300

    terminar as atividades 3,4 e 5

---
+++ atividade126.py
@@ -0,0 +1,5 @@
+lista_valores = [10.00,20.00,40.00,12.00,50.00]
+soma = 0
+for valor in lista_valores:
+    soma += valor 
+    print(f"total......{soma}")
---
+++ atividade226.py
@@ -0,0 +1,9 @@
+#  senha ao usuário
+tentativa= input("Digite a senha: ")
+senha = "12456"
+
+while tentativa != senha:
+    tentativa= input("Digite a senha novamente: ")
+    
+print("Bem-vindo")
+    
