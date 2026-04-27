922ea3a38f6a7d20b11ab75018a12af4576ddb88
Nycolas Antônio <nycolasla13@gmail.com>
2026-04-27 13:30:44 -0300

    terminar e corrigir

--- atividade22704.py
+++ atividade22704.py
@@ -1,14 +1,14 @@
 # Calculadora de Área de Terrenos (Geometria)
-def calcular_area(*largura,*comprimento):
+def calcular_area(*largura,comprimento):
+    calculo = 0
     for n in largura:
-        if n == 3:
-            multiplicacao1 = n * largura
-            multiplicacao2 = multiplicacao1 * comprimento
-    return multiplicacao2
+        calculo * n
+        return calculo
+    for n in comprimento:
+        calculo * n
+        return calculo
 
 lista_terrenos_largura = [10,5,12,2,15,0]
 lista_terrenos_comprimento = [20,5,30,6,50,3]
-Terrenos1 = int(input("Digite da área 1: "))
-Terrenos2 = int(input("Digite da área 2: "))
-Terrenos3 = int(input("Digite da área 3: "))
-mensagem = calcular_area(Terrenos)
+mensagem = calcular_area(lista_terrenos_largura, lista_terrenos_comprimento)
+print(f"área 1 {lista_terrenos_largura} área 2 {lista_terrenos_largura} área 3 {lista_terrenos_largura}. comprimento 1 {lista_terrenos_comprimento}, comprimento 2 {lista_terrenos_comprimento} comprimento 3 {lista_terrenos_comprimento}")
