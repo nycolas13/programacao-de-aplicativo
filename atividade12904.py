908f3994253a40773fa48f08cf46d8b66d22aa95
Nycolas Antônio <nycolasla13@gmail.com>
2026-04-30 10:12:44 -0300

    terminar a atividade 3, 4 e 5

--- atividade12904.py
+++ atividade12904.py
@@ -1,6 +1,14 @@
 # Sistema de Checkout com Imposto e Desconto
-def calcular_preco_final(valor_base,imposto_percentual,cupom_desconto)
-    valor = imposto_percentual + valor_base
-    subtrair = cupom_desconto - valor
-    if cupom_desconto > valor:
-        
+def calcular_preco_final(valor_base,imposto_percentual,cupom_desconto):
+    valor = valor_base - imposto_percentual
+    preco_final =  valor - cupom_desconto
+
+    if cupom_desconto > valor_base:
+        return 0
+        return preco_final
+
+usuario1 = int(input("Digite um valor: "))
+usuario2 = int(input("digite o imposto: "))
+usuario3 = int(input("Digite o cupom: "))
+mensagem = calcular_preco_final(usuario1,usuario2,usuario3)
+print(f"Preço final: {mensagem}")
